7b1584bb5abfcd08ab313c75c454bd6e08553406
Azizahaul <azizahauliarhamid@gmail.com>
2025-04-15 16:52:49 +0700

    mengisi array 2

--- array.cpp
+++ array.cpp
@@ -12,3 +12,8 @@ int main(){
     //mengisi array pada lokasi/index tertentu
     skor[2] = 25;
     cout << "Skor ke 3 = " << skor [2] << endl;
+    //mengisi array dengan loop for 
+    for(int i = 0; i < 3; i++){
+        cout << "Nama Mahasiswa ke-" << i+1 << " = ";
+        cin >> nama[i];
+    }
