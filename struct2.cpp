1c3309d77f3baa77eb8aeb95e58bb38c77dac31f
Azizahaul <azizahauliarhamid@gmail.com>
2025-04-15 16:58:20 +0700

    data mahasiswa 2

--- struct2.cpp
+++ struct2.cpp
@@ -11,3 +11,24 @@ struct mahasiswa{
     string nama;
     detailAlamat alamat;
 };
+
+int main(){
+    mahasiswa mhs;
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan Nama = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs.alamat.kota;
+
+    cout << endl;
+    cout << "Data Mahasiswa" << endl;
+
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat Desa = " << mhs.alamat.desa << endl;
+    cout << "Alamat Kota = " << mhs.alamat.kota << endl;
+}
